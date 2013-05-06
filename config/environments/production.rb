c02ac25cd09c817ceed4d1d72f04cc3c5df2ba17
NullVoxPopuli <LPSego3+dev@gmail.com>
2013-05-06 17:20:29 -0400

    fixed precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,10 @@ GenreMap::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = false
