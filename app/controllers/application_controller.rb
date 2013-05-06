2627e774207817af794f269936045275cac2c975
NullVoxPopuli <LPSego3+dev@gmail.com>
2013-05-06 03:18:38 -0400

    forget to remove teh toolbar creator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
  	protect_from_forgery
 
- 	before_filter :create_toolbar
-
  	def index
  		genres = Genre.where("parent_genre_id IS NULL")
 
@@ -30,20 +28,5 @@ class ApplicationController < ActionController::Base
  	def wiki
  		redirect_to "http://en.wikipedia.org/wiki/" + params[:page]
  	end
-
-protected
-	def create_toolbar
-		@toolbar = {
-			:home => "/",
-			:artists => artists_url,
-			:tracks => tracks_url,
-			:geners => genres_url,
-			:categories => categories_url,
-			:new_artist => new_artist_url,
-			:new_track => new_track_url,
-			:new_genre => new_genre_url,
-			:new_category => new_category_url
-		}
-	end
 
 end
